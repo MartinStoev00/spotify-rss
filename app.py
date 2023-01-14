951fc7fc44dd9c16586a4b3c9fdd6aaedcb67ca4
MartinStoev00 <martinstoev00@gmail.com>
2023-01-14 21:47:54 +0100

    add command

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def get_info(id):
     for elem in soup[0]:
         try:
             title = elem.div.select("div:nth-child(2)")[0].div.contents[0].contents[0].contents[-1]
+            link = elem.div.select("div:nth-child(2)")[0].div.contents[0]['href']
+            link = 'https://open.spotify.com' + link
+            command = 'gtep ' + link
             date = str(elem.div.select("div:nth-child(4)")[0].p.contents[0])
             datetime_object = None
             if date.endswith(str(year - 1)) or date.endswith(str(year)):
@@ -34,6 +37,8 @@ def get_info(id):
                 datetime_object = datetime.strptime(date, '%b %d').replace(year=year)
             item = ET.SubElement(channel, "item")
             ET.SubElement(item, "title").text = title
+            ET.SubElement(item, "link").text = link
+            ET.SubElement(item, "description").text = command
             ET.SubElement(item, "pubDate").text = datetime_object.strftime('%a, %d %b %Y 00:00:00')
         except AttributeError:
             break
